daa3949342164b65781bc5484ee8b5104f5a5205
Victor <muhitavictor69@gmail.com>
2023-10-10 23:10:56 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,13 +5,13 @@
 *Return: 0 (success)
 */
 
-int main(void) 
+int main(void)
 {
 int i;
-for(i = 0 ; i < 10 ; i++)
+for (i = 0 ; i < 10 ; i++)
 {
 putchar('0' + i);
 }
 putchar('\n');
-return 0;
+return (0);
 }
