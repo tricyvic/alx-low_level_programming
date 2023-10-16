d07e035a9ee5c82925a4072c8c925dda0c8a0f7b
Victor <muhitavictor69@gmail.com>
2023-10-16 13:31:21 -0700

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-*reset_to_98 - function that takes a pointer to an int as parameter and updates the value it points to to 98
+*reset_to_98 - takes a pointer to an int as parameter & updates the value it points to to 98
 *@n:- the value to be 98
-*Return: Always )
+*Return: Always 0
 */
 
 void reset_to_98(int *n)
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *int _strlen - returns the length of a string
+ *@s:- string to be checked
+ *Return: length of string
+ */
+
+int _strlen(char *s)
+{
+int string_length = 0;
+while (s[string_length])
+string_length ++;
+return (string_length);
+}
