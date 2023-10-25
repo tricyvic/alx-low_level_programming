4f46b2ebf422b8c4d19fed6b50f75503e6ce25a0
tricyvic <muhitavictor69@gmail.com>
2023-10-26 00:40:18 +0300

    3. You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strlen_recursion-Returns the length of a string
- * @s:The string to be measured 
+ * @s:The string to be measured
  * Return: The length of the string
  *
  */
