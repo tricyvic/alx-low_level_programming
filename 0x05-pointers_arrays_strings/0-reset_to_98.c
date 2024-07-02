f9561ec33cf50afd93e6612ef77f1bbd3fe4e254
Victor Muhita <muhitavictor69@gmail.com>
2024-07-02 04:41:00 -0700

    add file 0,1,2, and 3

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 -functuion that takes a pointer to an int a parameter
+ * and updates the value it points to to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
