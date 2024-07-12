dc9b6d2dc7d3d70f1fe89571318c05f61f79bb55
Victor Muhita <muhitavictor69@gmail.com>
2024-07-12 16:35:25 -0700

    add files

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - In Upercase Letters
+ * @c: 'char' to confirm
+ *
+ * Return: 0 / 1
+ */
+int _isupper(int c)
+{
+        	if (c >= 'A' && c <= 'Z')
+                    	return (1);
+        	else
+        	return (0);
+}
+
