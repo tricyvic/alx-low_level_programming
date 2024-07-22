cdc56d0ceb6f2eb655f569a3946f73915b0828fe
Victor Muhita <muhitavictor69@gmail.com>
2024-07-22 12:22:34 -0700

    finish 0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print name of the file
+ *
+ * ain - prints the name of the file
+ *
+ * Return: 0 (Success)
+ */
+int ain(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
