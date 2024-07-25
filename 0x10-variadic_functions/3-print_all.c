343f3d0f6bf23747c53a50e1f87adadb1f18535b
Victor Muhita <muhitavictor69@gmail.com>
2024-07-25 11:05:39 -0700

    redo 3-print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,11 +33,11 @@ void print_all(const char * const format, ...)
 					str = va_arg(list, char *);
 					if (!str)
 						str = "(nil)";
-						printf("%s%s", sep, str);
+					printf("%s%s", sep, str);
 					break;
 				default:
 					i++;
-				continue;
+					continue;
 			}
 			sep = ", ";
 			i++;
